234ddc48277d61719f67236fb4781ce7a1ff728f
Azizbek <PROUZBMASTER>
2023-05-17 18:45:53 +0500

    end

---
+++ console/script.js
@@ -0,0 +1,13 @@
+let a = prompt("Yoshingizni kiriting")
+let b = 18
+
+if ( a >= b ) {
+    alert("Siz armiyaga borishingiz kerak")
+}
+else if (a <= 0){
+    alert("Kiritilmadi")
+}
+else if ( b > a) {
+    let c = 18 - a
+    alert("Siz armiyaga borishingizga " + c + " yil bor")
+}                               
